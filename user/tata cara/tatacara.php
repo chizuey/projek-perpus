4c89c523215d527cb15c7449e062cc3d1fbcffe3
ahmadzeinfathurrahman-design <ahmadzeinfathurrahman@gmail.com>
2026-04-20 17:19:52 +0700

    first commit zein

---
+++ user/tata cara/tatacara.php
@@ -0,0 +1,75 @@
+
+    <!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="stylecara.css">
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<div class="panduan-tata-cara" ><div class="main" ><div class="hero-section" ><div class="container" ><div class="overlay-border-wrapper" ><div class="overlay-border" ><img class="checklist" src="gambar/cara.png" /></div></div>
+<div class="heading" ><div class="text-wrapper" >Tata Cara</div></div>
+<div class="div-wrapper" ><div class="text" >Perpustakaan POLIJE</div></div></div></div></div>
+<div class="container-18" ><div class="section" ><div class="background" ><img class="background-2" src="gambar/buku back.png" />
+<div class="div-wrapper-3" ><p class="text-wrapper-7" >I. Tata Cara Peminjaman Buku</p></div></div>
+<div class="container-19" ><div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-13" >1</div></div>
+<div class="container-21" ><p class="mahasiswa-datang-ke" >Mahasiswa datang ke&nbsp;&nbsp;perpustakaan untuk meminjam buku dan harus memiliki kartu peminjam.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-14" >2</div></div>
+<div class="container-21" ><p class="text-wrapper-8" >Mahasiswa mencari buku yang ingin dipinjam di rak perpustakaan.</p></div></div>
+<div class="container-22" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-15" >3</div></div>
+<div class="container-23" ><p class="setelah-buku" ><br/> Setelah buku ditemukan, mahasiswa menuju ke meja petugas untuk melakukan peminjaman.</p></div></div>
+<div class="container-24" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-16" >4</div></div>
+<div class="container-25" ><p class="text-wrapper-9" >Petugas kemudian mengisi data buku pada kartu peminjam.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-17" >5</div></div>
+<div class="container-21" ><p class="text-wrapper-10" >Jika sudah sesuai, kartu peminjam disimpan di loker sesuai nomor urut yang telah ditentukan.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-18" >6</div></div>
+<div class="container-21" ><p class="setelah-itu-petugas" > Setelah itu, petugas memasukkan data peminjaman ke sistem perpustakaan.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-19" >7</div></div>
+<div class="container-26" ><p class="text-wrapper-11" > Petugas memasukkan ID peminjam (misalnya NIM mahasiswa) ke dalam sistem sirkulasi pada bagian peminjaman.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-wrapper-12" >8</div></div>
+<div class="container-21" ><p class="text-wrapper-13" > Selanjutnya petugas mengetik barcode buku secara manual ke dalam sistem.</p></div></div>
+<div class="container-27" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-wrapper-12" >9</div></div>
+<div class="container-21" ><p class="text-wrapper-13" > Sistem akan menampilkan informasi tanggal pengembalian buku.</p></div></div>
+<div class="container-27" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-wrapper-14" >10</div></div>
+<div class="container-21" ><p class="jika-semua-data" >&nbsp;&nbsp;Jika semua data sudah benar, petugas menekan klik selesai untuk menyimpan transaksi peminjaman.</p></div></div></div></div>
+<div class="section-2" ><div class="background" ><img class="background-2" src="gambar/buku back.png" />
+<div class="div-wrapper-3" ><p class="text-wrapper-7" >II. Tata Cara Pengembalian Buku</p></div></div>
+<div class="container-19" ><div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-13" >1</div></div>
+<div class="container-21" ><p class="mahasiswa-datang-ke" > Mahasiswa datang ke perpustakaan dengan membawa buku yang telah dipinjam.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-14" >2</div></div>
+<div class="container-21" ><p class="text-wrapper-8" >Mahasiswa menyerahkan buku kepada petugas.</p></div></div>
+<div class="container-22" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-15" >3</div></div>
+<div class="container-23" ><p class="text-wrapper-15" >Petugas mencari kartu peminjam di loker berdasarkan nomor urut tanggal pengembalian.</p></div></div>
+<div class="container-24" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-16" >4</div></div>
+<div class="container-25" ><p class="text-wrapper-9" > Petugas mencari kartu peminjam di loker berdasarkan nomor urut tanggal pengembalian.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-17" >5</div></div>
+<div class="container-21" ><p class="text-wrapper-16" >Jika terdapat denda keterlambatan, petugas akan memberitahukan jumlah denda kepada mahasiswa.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-18" >6</div></div>
+<div class="container-21" ><p class="setelah-proses" > Setelah proses pengembalian sudah dilakukan di sistem, petugas akan menekan klik kembalikan.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-19" >7</div></div>
+<div class="container-26" ><p class="text-wrapper-11" >Petugas kemudian memberikan kembali kartu peminjam kepada mahasiswa.</p></div></div>
+<div class="container-20" ><div class="background-border" ><div class="overlay-shadow" ></div>
+<div class="text-wrapper-12" >8</div></div>
+<div class="container-21" ><p class="text-wrapper-13" > Selanjutnya petugas mengetik barcode buku secara manual ke dalam sistem.</p></div></div></div></div></div></div>
+<?php include 'foot.php'; ?>
+</body>
+</html>
